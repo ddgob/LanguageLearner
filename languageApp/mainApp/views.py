27a6b7bd4ed884348933099de8fbbe522def7323
Daniel Barbosa <danielbarbosa@Daniels-MacBook-Pro.local>
2024-01-25 23:07:05 -0300

    Created read content and flashcard views

--- languageApp/mainApp/views.py
+++ languageApp/mainApp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Content
+from .models import Flashcards
 # Create your views here.
 
-def content(request):
-    return render(request, "mainApp/content.html")
+def read_content(request):
+    contents = Content.objects.all()
+    return render(request, "mainApp/read_content.html", {'contents': contents})
+
+def flashcards(request):
+    flashcards = Flashcards.objects.all()
+    return render(request, "mainApp/flashcards.html", {'flashcards': flashcards})
