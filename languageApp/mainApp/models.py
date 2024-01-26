0d447b52ae28f7a1c2347be5286c121e2d73128a
Frederico Baker <fredericodsbaker@gmail.com>
2024-01-25 23:03:11 -0300

    Models finished

--- languageApp/mainApp/models.py
+++ languageApp/mainApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Content(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+class Flashcard(models.Model):
+    originalText = models.CharField(max_length=250)
+    translatedText = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.originalText
